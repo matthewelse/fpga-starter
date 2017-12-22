54be6d4aac19f7c4037fef743afd52b69e1ad2a7
Matthew Else <matthewelse1997@gmail.com>
2017-12-22 14:23:14 +0000

    Tidy up toplevel.sv

--- toplevel.sv
+++ toplevel.sv
@@ -1,45 +1,13 @@
-//
-// Copyright (c) 2015 A. Theodore Markettos
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-//
-// THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
-// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-// OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-// SUCH DAMAGE.
-
-// Top level file for DE1-SoC board with
-// Cambridge display board
-
-// Uncomment this if you have an HPS (ARM CPU) in your design
+/* Copyright 2017 Matthew Else */
 `define ENABLE_HPS
 
 module toplevel(
-
-`ifdef ENABLE_ADC
       // Analogue-digital converter
       inout              ADC_CS_N,
       output             ADC_DIN,
       input              ADC_DOUT,
       output             ADC_SCLK,
-`endif // ENABLE_ADC
 
-`ifdef ENABLE_DAC
       // Audio DAC
       input              AUD_ADCDAT,
       inout              AUD_ADCLRCK,
@@ -47,17 +15,13 @@ module toplevel(
       output             AUD_DACDAT,
       inout              AUD_DACLRCK,
       output             AUD_XCK,
-`endif // ENABLE_DAC
 
       // Clocks
       input              CLOCK_50,
-`ifdef ENABLE_CLOCKS
       input              CLOCK2_50,
       input              CLOCK3_50,
       input              CLOCK4_50,
-`endif
 
-`ifdef ENABLE_SDRAM
       // FPGA-side SDRAM
       output      [12:0] DRAM_ADDR,
       output      [1:0]  DRAM_BA,
@@ -70,25 +34,17 @@ module toplevel(
       output             DRAM_RAS_N,
       output             DRAM_UDQM,
       output             DRAM_WE_N,
-`endif // ENABLE_SDRAM
 
-`ifdef ENABLE_FAN
       // Fan control (unused on native board)
       output             FAN_CTRL,
-`endif
 
-`ifdef ENABLE_I2C
       // FPGA I2C
       output             FPGA_I2C_SCLK,
       inout              FPGA_I2C_SDAT,
-`endif
 
-`ifdef ENABLE_GPIO
       // General purpose I/O
       inout     [35:0]         GPIO_0,
-`endif  
- 
-`ifdef ENABLE_HEX
+
       // Hex LEDs
       output      [6:0]  HEX0,
       output      [6:0]  HEX1,
@@ -96,7 +52,6 @@ module toplevel(
       output      [6:0]  HEX3,
       output      [6:0]  HEX4,
       output      [6:0]  HEX5,
-`endif // ENABLE_HEX
 		
 `ifdef ENABLE_HPS
       // ARM Cortex A9 Hard Processor System
@@ -154,43 +109,32 @@ module toplevel(
       output             HPS_USB_STP,
 `endif /*ENABLE_HPS*/
 
-`ifdef ENABLE_IR
       // Infra-red
       input              IRDA_RXD,
       output             IRDA_TXD,
-`endif // ENABLE_IR
 
-`ifdef ENABLE_KEY
       // Push buttons on DE1-SoC mainboard
       input       [3:0]  KEY,
-`endif
 
       // Red LED row
       output      [9:0]  LEDR
 
-`ifdef ENABLE_PS2
       // PS2 port
       inout              PS2_CLK,
       inout              PS2_CLK2,
       inout              PS2_DAT,
       inout              PS2_DAT2,
-`endif
 
-`ifdef ENABLE_SW
       // Slide switches
       input       [9:0]  SW,
-`endif
 
-`ifdef ENABLE_TMDS
       // TMDS
       input              TD_CLK27,
       input      [7:0]  TD_DATA,
       input             TD_HS,
       output             TD_RESET_N,
       input             TD_VS,
-`endif
 
-`ifdef ENABLE_VGA
       // VGA video
       output      [7:0]  VGA_B,
       output             VGA_BLANK_N,
@@ -200,41 +144,6 @@ module toplevel(
       output      [7:0]  VGA_R,
       output             VGA_SYNC_N,
       output             VGA_VS,
-`endif
-      // Cambridge display board (plugged into GPIO1 port)
-
-`ifdef ENABLE_CAM
-      // rotary dials
-      input       [1:0]  DIALL,
-      input       [1:0]  DIALR,
-      // LED pixel ring (inverted before reaching ring)
-      output             LEDRINGn,
-      
-      // LCD display
-      output      [7:0]  LCD_R,
-      output      [7:0]  LCD_G,
-      output      [7:0]  LCD_B,
-      // -- only LCD_R[7:2], LCD_G[7:1], LCD_B[7:2] are wired
-      // through to display board, low-order pins are ignored
-        
-      output             LCD_HSYNC,
-      output             LCD_VSYNC,
-      output             LCD_DEN,
-      output             LCD_DCLK,
-      output             LCD_ON,	    // set high to enable LCD panel
-      output             LCD_BACKLIGHT, // set high to turn on backlight, PWM to dim
-      
-      // shift register for buttons on display board
-      output             SHIFT_CLKIN,
-      output             SHIFT_LOAD,
-      input              SHIFT_OUT,
-
-      // capacitive touch sensor reset (high=enabled)
-      output             TOUCH_WAKE,
-      // I2C for touch, temperature and EEPROM
-      inout              DISPLAY_SDA,
-      inout              DISPLAY_SCL
-`endif
 );
 
 // code goes here
@@ -244,64 +153,64 @@ always @(posedge CLOCK_50) begin
 	count <= count + 1;
 end
 
-assign LEDR[0] = count[20];
+assign LEDR[1:0] = SW[1:0];
 
 
-    starter u0 (
-        .clk_clk                (CLOCK_50),         //             clk.clk
-        .leds_connection_export (LEDR[9:1]),        // leds_connection.export
-        .memory_mem_a           (HPS_DDR3_ADDR),    //          memory.mem_a
-        .memory_mem_ba          (HPS_DDR3_BA),      //                .mem_ba
-        .memory_mem_ck          (HPS_DDR3_CK_P),    //                .mem_ck
-        .memory_mem_ck_n        (HPS_DDR3_CK_N),    //                .mem_ck_n
-        .memory_mem_cke         (HPS_DDR3_CKE),     //                .mem_cke
-        .memory_mem_cs_n        (HPS_DDR3_CS_N),    //                .mem_cs_n
-        .memory_mem_ras_n       (HPS_DDR3_RAS_N),   //                .mem_ras_n
-        .memory_mem_cas_n       (HPS_DDR3_CAS_N),   //                .mem_cas_n
-        .memory_mem_we_n        (HPS_DDR3_WE_N),    //                .mem_we_n
-        .memory_mem_reset_n     (HPS_DDR3_RESET_N), //                .mem_reset_n
-        .memory_mem_dq          (HPS_DDR3_DQ),      //                .mem_dq
-        .memory_mem_dqs         (HPS_DDR3_DQS_P),   //                .mem_dqs
-        .memory_mem_dqs_n       (HPS_DDR3_DQS_N),   //                .mem_dqs_n
-        .memory_mem_odt         (HPS_DDR3_ODT),     //                .mem_odt
-        .memory_mem_dm          (HPS_DDR3_DM),      //                .mem_dm
-        .memory_oct_rzqin       (HPS_DDR3_RZQ),     //                .oct_rzqin
-        .reset_reset_n          (1),                 //           reset.reset_n
-		    .hps_io_hps_io_emac1_inst_TX_CLK (HPS_ENET_GTX_CLK), //          hps_io.hps_io_emac1_inst_TX_CLK
-        .hps_io_hps_io_emac1_inst_TXD0   (HPS_ENET_TX_DATA[0]),   //                .hps_io_emac1_inst_TXD0
-        .hps_io_hps_io_emac1_inst_TXD1   (HPS_ENET_TX_DATA[1]),   //                .hps_io_emac1_inst_TXD1
-        .hps_io_hps_io_emac1_inst_TXD2   (HPS_ENET_TX_DATA[2]),   //                .hps_io_emac1_inst_TXD2
-        .hps_io_hps_io_emac1_inst_TXD3   (HPS_ENET_TX_DATA[3]),   //                .hps_io_emac1_inst_TXD3
-        .hps_io_hps_io_emac1_inst_RXD0   (HPS_ENET_RX_DATA[0]),   //                .hps_io_emac1_inst_RXD0
-        .hps_io_hps_io_emac1_inst_MDIO   (HPS_ENET_MDIO),   //                .hps_io_emac1_inst_MDIO
-        .hps_io_hps_io_emac1_inst_MDC    (HPS_ENET_MDC),    //                .hps_io_emac1_inst_MDC
-        .hps_io_hps_io_emac1_inst_RX_CTL (HPS_ENET_RX_DV), //                .hps_io_emac1_inst_RX_CTL
-        .hps_io_hps_io_emac1_inst_TX_CTL (HPS_ENET_TX_EN), //                .hps_io_emac1_inst_TX_CTL
-        .hps_io_hps_io_emac1_inst_RX_CLK (HPS_ENET_RX_CLK), //                .hps_io_emac1_inst_RX_CLK
-        .hps_io_hps_io_emac1_inst_RXD1   (HPS_ENET_RX_DATA[1]),   //                .hps_io_emac1_inst_RXD1
-        .hps_io_hps_io_emac1_inst_RXD2   (HPS_ENET_RX_DATA[2]),   //                .hps_io_emac1_inst_RXD2
-        .hps_io_hps_io_emac1_inst_RXD3   (HPS_ENET_RX_DATA[3]),   //                .hps_io_emac1_inst_RXD3
-        .hps_io_hps_io_sdio_inst_CMD     (HPS_SD_CMD),     //                .hps_io_sdio_inst_CMD
-        .hps_io_hps_io_sdio_inst_D0      (HPS_SD_DATA[0]),      //                .hps_io_sdio_inst_D0
-        .hps_io_hps_io_sdio_inst_D1      (HPS_SD_DATA[1]),      //                .hps_io_sdio_inst_D1
-        .hps_io_hps_io_sdio_inst_CLK     (HPS_SD_CLK),     //                .hps_io_sdio_inst_CLK
-        .hps_io_hps_io_sdio_inst_D2      (HPS_SD_DATA[2]),      //                .hps_io_sdio_inst_D2
-        .hps_io_hps_io_sdio_inst_D3      (HPS_SD_DATA[3]),      //                .hps_io_sdio_inst_D3
-        .hps_io_hps_io_usb1_inst_D0      (HPS_USB_DATA[0]),      //                .hps_io_usb1_inst_D0
-        .hps_io_hps_io_usb1_inst_D1      (HPS_USB_DATA[1]),      //                .hps_io_usb1_inst_D1
-        .hps_io_hps_io_usb1_inst_D2      (HPS_USB_DATA[2]),      //                .hps_io_usb1_inst_D2
-        .hps_io_hps_io_usb1_inst_D3      (HPS_USB_DATA[3]),      //                .hps_io_usb1_inst_D3
-        .hps_io_hps_io_usb1_inst_D4      (HPS_USB_DATA[4]),      //                .hps_io_usb1_inst_D4
-        .hps_io_hps_io_usb1_inst_D5      (HPS_USB_DATA[5]),      //                .hps_io_usb1_inst_D5
-        .hps_io_hps_io_usb1_inst_D6      (HPS_USB_DATA[6]),      //                .hps_io_usb1_inst_D6
-        .hps_io_hps_io_usb1_inst_D7      (HPS_USB_DATA[7]),      //                .hps_io_usb1_inst_D7
-        .hps_io_hps_io_usb1_inst_CLK     (HPS_USB_CLKOUT),     //                .hps_io_usb1_inst_CLK
-        .hps_io_hps_io_usb1_inst_STP     (HPS_USB_STP),     //                .hps_io_usb1_inst_STP
-        .hps_io_hps_io_usb1_inst_DIR     (HPS_USB_DIR),     //                .hps_io_usb1_inst_DIR
-        .hps_io_hps_io_usb1_inst_NXT     (HPS_USB_NXT),     //                .hps_io_usb1_inst_NXT
-        .hps_io_hps_io_uart0_inst_RX     (HPS_UART_RX),     //                .hps_io_uart0_inst_RX
-        .hps_io_hps_io_uart0_inst_TX     (HPS_UART_TX)      //                .hps_io_uart0_inst_TX
-    );
+starter u0 (
+  .clk_clk                (CLOCK_50),                      //             clk.clk
+  .leds_connection_export (LEDR[9:2]),                     // leds_connection.export
+  .memory_mem_a           (HPS_DDR3_ADDR),                 //          memory.mem_a
+  .memory_mem_ba          (HPS_DDR3_BA),                   //                .mem_ba
+  .memory_mem_ck          (HPS_DDR3_CK_P),                 //                .mem_ck
+  .memory_mem_ck_n        (HPS_DDR3_CK_N),                 //                .mem_ck_n
+  .memory_mem_cke         (HPS_DDR3_CKE),                  //                .mem_cke
+  .memory_mem_cs_n        (HPS_DDR3_CS_N),                 //                .mem_cs_n
+  .memory_mem_ras_n       (HPS_DDR3_RAS_N),                //                .mem_ras_n
+  .memory_mem_cas_n       (HPS_DDR3_CAS_N),                //                .mem_cas_n
+  .memory_mem_we_n        (HPS_DDR3_WE_N),                 //                .mem_we_n
+  .memory_mem_reset_n     (HPS_DDR3_RESET_N),              //                .mem_reset_n
+  .memory_mem_dq          (HPS_DDR3_DQ),                   //                .mem_dq
+  .memory_mem_dqs         (HPS_DDR3_DQS_P),                //                .mem_dqs
+  .memory_mem_dqs_n       (HPS_DDR3_DQS_N),                //                .mem_dqs_n
+  .memory_mem_odt         (HPS_DDR3_ODT),                  //                .mem_odt
+  .memory_mem_dm          (HPS_DDR3_DM),                   //                .mem_dm
+  .memory_oct_rzqin       (HPS_DDR3_RZQ),                  //                .oct_rzqin
+  .reset_reset_n          (1),                             //           reset.reset_n
+  .hps_io_hps_io_emac1_inst_TX_CLK (HPS_ENET_GTX_CLK),     //          hps_io.hps_io_emac1_inst_TX_CLK
+  .hps_io_hps_io_emac1_inst_TXD0   (HPS_ENET_TX_DATA[0]),  //                .hps_io_emac1_inst_TXD0
+  .hps_io_hps_io_emac1_inst_TXD1   (HPS_ENET_TX_DATA[1]),  //                .hps_io_emac1_inst_TXD1
+  .hps_io_hps_io_emac1_inst_TXD2   (HPS_ENET_TX_DATA[2]),  //                .hps_io_emac1_inst_TXD2
+  .hps_io_hps_io_emac1_inst_TXD3   (HPS_ENET_TX_DATA[3]),  //                .hps_io_emac1_inst_TXD3
+  .hps_io_hps_io_emac1_inst_RXD0   (HPS_ENET_RX_DATA[0]),  //                .hps_io_emac1_inst_RXD0
+  .hps_io_hps_io_emac1_inst_MDIO   (HPS_ENET_MDIO),        //                .hps_io_emac1_inst_MDIO
+  .hps_io_hps_io_emac1_inst_MDC    (HPS_ENET_MDC),         //                .hps_io_emac1_inst_MDC
+  .hps_io_hps_io_emac1_inst_RX_CTL (HPS_ENET_RX_DV),       //                .hps_io_emac1_inst_RX_CTL
+  .hps_io_hps_io_emac1_inst_TX_CTL (HPS_ENET_TX_EN),       //                .hps_io_emac1_inst_TX_CTL
+  .hps_io_hps_io_emac1_inst_RX_CLK (HPS_ENET_RX_CLK),      //                .hps_io_emac1_inst_RX_CLK
+  .hps_io_hps_io_emac1_inst_RXD1   (HPS_ENET_RX_DATA[1]),  //                .hps_io_emac1_inst_RXD1
+  .hps_io_hps_io_emac1_inst_RXD2   (HPS_ENET_RX_DATA[2]),  //                .hps_io_emac1_inst_RXD2
+  .hps_io_hps_io_emac1_inst_RXD3   (HPS_ENET_RX_DATA[3]),  //                .hps_io_emac1_inst_RXD3
+  .hps_io_hps_io_sdio_inst_CMD     (HPS_SD_CMD),           //                .hps_io_sdio_inst_CMD
+  .hps_io_hps_io_sdio_inst_D0      (HPS_SD_DATA[0]),       //                .hps_io_sdio_inst_D0
+  .hps_io_hps_io_sdio_inst_D1      (HPS_SD_DATA[1]),       //                .hps_io_sdio_inst_D1
+  .hps_io_hps_io_sdio_inst_CLK     (HPS_SD_CLK),           //                .hps_io_sdio_inst_CLK
+  .hps_io_hps_io_sdio_inst_D2      (HPS_SD_DATA[2]),       //                .hps_io_sdio_inst_D2
+  .hps_io_hps_io_sdio_inst_D3      (HPS_SD_DATA[3]),       //                .hps_io_sdio_inst_D3
+  .hps_io_hps_io_usb1_inst_D0      (HPS_USB_DATA[0]),      //                .hps_io_usb1_inst_D0
+  .hps_io_hps_io_usb1_inst_D1      (HPS_USB_DATA[1]),      //                .hps_io_usb1_inst_D1
+  .hps_io_hps_io_usb1_inst_D2      (HPS_USB_DATA[2]),      //                .hps_io_usb1_inst_D2
+  .hps_io_hps_io_usb1_inst_D3      (HPS_USB_DATA[3]),      //                .hps_io_usb1_inst_D3
+  .hps_io_hps_io_usb1_inst_D4      (HPS_USB_DATA[4]),      //                .hps_io_usb1_inst_D4
+  .hps_io_hps_io_usb1_inst_D5      (HPS_USB_DATA[5]),      //                .hps_io_usb1_inst_D5
+  .hps_io_hps_io_usb1_inst_D6      (HPS_USB_DATA[6]),      //                .hps_io_usb1_inst_D6
+  .hps_io_hps_io_usb1_inst_D7      (HPS_USB_DATA[7]),      //                .hps_io_usb1_inst_D7
+  .hps_io_hps_io_usb1_inst_CLK     (HPS_USB_CLKOUT),       //                .hps_io_usb1_inst_CLK
+  .hps_io_hps_io_usb1_inst_STP     (HPS_USB_STP),          //                .hps_io_usb1_inst_STP
+  .hps_io_hps_io_usb1_inst_DIR     (HPS_USB_DIR),          //                .hps_io_usb1_inst_DIR
+  .hps_io_hps_io_usb1_inst_NXT     (HPS_USB_NXT),          //                .hps_io_usb1_inst_NXT
+  .hps_io_hps_io_uart0_inst_RX     (HPS_UART_RX),          //                .hps_io_uart0_inst_RX
+  .hps_io_hps_io_uart0_inst_TX     (HPS_UART_TX)           //                .hps_io_uart0_inst_TX
+);
 
 
 
